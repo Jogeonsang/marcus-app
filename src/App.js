5a0f336d3daf93de19690e974eec24030767e5f7
Jogeonsang <rhcn258@gmail.com>
2018-06-30 17:51:13 +0900

    초기화
    create-react-app 초기화 설정

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      시작한다
+      </div>
+    );
+  }
+}
+
+export default App;
