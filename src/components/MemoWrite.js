c87dac18a70b9d0bacd40ef5dac027a69fb31915
Jogeonsang <rhcn258@gmail.com>
2018-07-18 17:00:44 +0900

    Write Component 안에 메모 스타일을 바꿀 수 있는 버튼 추가

--- src/components/MemoWrite.js
+++ src/components/MemoWrite.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './MemoWrite.css';
 
-const MemoWrite = ({value, onChange,selectedContent }) => {
+const MemoWrite = ({value, onChange,selectedContent, onTodo }) => {
   return (
     <div className="Write">
-      <textarea
-        placeholder="title"
+      <textarea className = "Title"
+        placeholder="H1"
         name={selectedContent.id}
         value={selectedContent.text}
         onChange={onChange}
       />
+      <div className="CheckList" onClick={() => onTodo}>
+      </div>
     </div>
   );
 };
